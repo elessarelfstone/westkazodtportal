81f26ed65c318258461864226a956d3bc6cd1bbd
dake <elessarelfstone@mail.ru>
2017-04-17 15:43:31 +0500

    добавленаа начальная заливка лицевых счетов

--- migration/db_migration.py
+++ migration/db_migration.py
@@ -1,76 +1,105 @@
 import cx_Oracle
 import psycopg2
 from configparser import ConfigParser
+import logging
 
+class DbMigration2:
 
-class DbMigration:
-    def __init__(self, settings, section):
-        parser = ConfigParser()
-        parser.read(settings)
-        _conn_settings = {}
-        if parser.has_section(section):
-            params = parser.items(section)
-            for param in params:
-                _conn_settings[param[0]] = param[1]
-        else:
-            raise Exception('Секция отсутсвует')
-        self._conn_settings = _conn_settings
+    def __init__(self, settings):
 
+        """
+         получение параметров подключений у обеих баз из ini файла
+         установка подключений
+         установка праметров логирования
 
-class DbOracle(DbMigration):
-    def __init__(self, settings, section):
-        super(DbOracle, self).__init__(settings, section)
-        self._dsn = cx_Oracle.makedsn(self._conn_settings["host"], self._conn_settings["port"], self._conn_settings["sid"])
-        self._conn = cx_Oracle.connect(self._conn_settings["user"], self._conn_settings["password"], self._dsn)
+        """
 
-    def get_rows(self, sql):
+        # настройка логирования
+        self._application_name = "Telecom_Storage"
+        self.logger = logging.getLogger(self._application_name)
+        self.logger.setLevel(logging.INFO)
+
+        formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+
+        file_handler = logging.FileHandler('storage.log')
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(formatter)
+
+        console = logging.StreamHandler()
+        console.setLevel(logging.WARNING)
+        console.setFormatter(formatter)
+
+        self.logger.addHandler(console)
+        self.logger.addHandler(file_handler)
+
+        self.conditions = ()
+
         try:
-            res = []
-            curs = self._conn.cursor()
-            curs.execute(sql)
-            colnames = [desc[0] for desc in curs.description]
-            for row in curs.fetchall():
-                rw = {}
-                for name, value in zip(colnames, row):
-                    rw[name] = value
-                res.append(rw)
-            return res
-        finally:
-            self._conn.close()
+            # получение настроек из ini файлов
+            parser = ConfigParser()
+            parser.read(settings)
+            _conn_oracle_sett = {}
+            _conn_postgres_sett = {}
+            if parser.has_section('oracle') and parser.has_section('postgres'):
+                params = parser.items('oracle')
+                for param in params:
+                    _conn_oracle_sett[param[0]] = param[1]
+                params = parser.items('postgres')
+                for param in params:
+                    _conn_postgres_sett[param[0]] = param[1]
+            else:
+                raise Exception('Секции отсуствуют')
+            self._conn_oracle_sett = _conn_oracle_sett
+            self._conn_postgres_sett = _conn_postgres_sett
+            # создание подключения к базе oracle
 
-    def get_rows_as_tuple(self, sql):
+            self._dsn = cx_Oracle.makedsn(self._conn_oracle_sett["host"], self._conn_oracle_sett["port"],
+                                      self._conn_oracle_sett["sid"])
+            self._oracle_conn = cx_Oracle.connect(self._conn_oracle_sett["user"], self._conn_oracle_sett["password"],
+                                              self._dsn)
+
+            self._conn_postgres_string = " ".join(["%s=%s" % (k, v) for k, v in self._conn_postgres_sett.items()])
+            # print(self._conn_string)
+            self._postgres_conn = psycopg2.connect(self._conn_postgres_string)
+        except Exception as e:
+            self.logger.exception(e)
+
+    def export_abonents(self):
+        """ Производит заливку лицевых счетов из БИТТл в таблицы хранилища """
         try:
-            res = []
-            curs = self._conn.cursor()
-            curs.execute(sql)
-            for row in curs.fetchall():
-                res.append(row)
-            return res
+            # получаем лицевые счета абонентов из БИТТл
+            self.logger.warning('Старт процедуры экспорта лицевых счетов абонентов')
+            or_cur = self._oracle_conn.cursor()
+            sql = "select id from db.abonent"
+            or_cur.execute(sql)
+
+            # abns = [ abn[0] for abn in or_cur.fetchall()]
+            abns = or_cur.fetchall()
+            self.logger.info('Выбрано лицевых счетов в количестве '+ str(len(abns)))
+            pg_cur = self._postgres_conn.cursor()
+            # получаем список таблиц в которые будем заливать лицевые счета абонентов с БИТТл
+            sql = "select table_name from storage.data_type dt where dt.base = %s"
+            pg_cur.execute(sql, ("ABN", ))
+            tables = [tp[0] for tp in pg_cur.fetchall()]
+
+            #  обнуляем эти таблицы и заливаем туда лицевые счета
+            for tbl in tables:
+                sql = 'truncate table '+ tbl
+                pg_cur.execute(sql)
+                self.logger.info('Таблица ' + tbl + ' обнулена')
+                sql = "insert into "+tbl+"(abonent_id) values(%s)"
+                pg_cur.executemany(sql, abns)
+                #rcnt = pg_cur.rowcount
+                self.logger.info('В таблицу ' + tbl + ' всталено ' + str(pg_cur.rowcount) + ' записей. ')
+
+            self._postgres_conn.commit()
+
+            self.logger.warning('Процедура экспорта лицевых счетов абонетов завершена')
+        except Exception as e:
+            self.logger.exception(e)
         finally:
-            self._conn.close()
+            self._oracle_conn.close()
+            self._postgres_conn.close()
 
 
-class DbPostgreSQL(DbMigration):
-    def __init__(self, settings, section):
-        super(DbPostgreSQL, self).__init__(settings, section)
-        self._conn_string = " ".join(["%s=%s" % (k, v) for k, v in self._conn_settings.items()])
-        #print(self._conn_string)
-        self._conn = psycopg2.connect(self._conn_string)
-
-    def get_version_db(self):
-        try:
-            cur = self._conn.cursor()
-            cur.execute('SELECT version()')
-            db_version = cur.fetchone()
-            return db_version
-        finally:
-            self._conn.close()
-
-    def insert_rows(self, sql, rows):
-        try:
-            cur = self._conn.cursor()
-            cur.executemany(sql, rows)
-            self._conn.commit()
-            cur.close()
-        finally:
-            self._conn.close()
+    #def get_prepared_script(self, template, ):
--- migration/main.py
+++ migration/main.py
@@ -3,11 +3,5 @@ import db_migration_utils as utils
 
 
 with utils.Profiler() as p:
-    #source = dbmigr.DbOracle("database.ini", "oracle")
-    #twns = source.get_rows_as_tuple("select * from db.town")
-
-    #abonents = source.get_rows_as_tuple("select id, name from db.abonent")
-    destination = dbmigr.DbPostgreSQL("database.ini", "postgresql")
-    version = destination.get_version_db()
-    print(version)
-    #destination.insert_rows("insert into abonent(id, name) values(%s, %s)", abonents)
+    migration = dbmigr.DbMigration2("database.ini")
+    migration.export_abonents()
